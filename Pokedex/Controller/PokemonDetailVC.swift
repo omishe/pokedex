716eaddfe854053f77a8ae2a94321ec47255fdfc
Omishe <omishe@hotmail.com>
2026-05-03 23:15:45 -0600

    Se agrega la funcionalidad de barra de busqueda

---
+++ Pokedex/Controller/PokemonDetailVC.swift
@@ -0,0 +1,29 @@
+//
+//  PokemonDetailVC.swift
+//  Pokedex
+//
+//  Created by Omishe Hernandez on 03/05/26.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
